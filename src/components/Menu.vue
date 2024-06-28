6c13f4ffbac8c6d2cf9e30710f3c4ad1f567f944
Павел Панченко <p.panchenko@clinic23.ru>
2024-06-28 16:36:44 +0300

    Небольшое изменение элемента профиля, перенос проекта с рабочего компа

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -6,12 +6,20 @@ defineProps({
 
 <template>
   <div class="flex justify-center bg-amber-100 ml-12 mr-12">
-    <div class="inline-flex justify-center gap-10 w-4/5">
-      <a href="#"><p>Главная</p></a>
-      <a href="#"><p>Новые</p></a>
-      <a href="#"><p>Мои запросы</p></a>
-      <div class="w-2/5 flex justify-end">
-        <a href="#"><p>{{ name }}</p></a>
+    <div class="inline-flex justify-center w-4/5">
+      <div class="inline-flex gap-10 justify-start">
+        <a href="#"><p>Главная</p></a>
+        <a href="#"><p>Новые</p></a>
+        <a href="#"><p>Мои запросы</p></a>
+      </div>
+      <div class="w-1/2 flex justify-end">
+        <a href="#">
+          <div class="inline-flex gap-4">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkthDzK4If2hjtRw9Qq02RtMYOtHvQ3bNtug&s"
+                 alt="avatar" class="w-8 h-8 rounded-full">
+            <p>{{ name }}</p>
+          </div>
+        </a>
       </div>
     </div>
   </div>
